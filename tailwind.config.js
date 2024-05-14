2e3a3fc61274daedbc120ffb72825ec410a49e65
Jose Cavazos <jose.cavazos@clubers.com>
2024-05-14 12:48:11 -0600

    feat(html): generated layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'b': '#27374D',
+        'bb': '#526D82',
+        'bbb': '#9DB2BF',
+        'bbbb': '#DDE6ED'
+      },
+      boxShadow: {
+        'right': '15px 0px 5px -5px rgba(0,0,0,0.3)',
+        'left': '-15px 0px 5px -5px rgba(0,0,0,0.3)'
+      }
+    },
   },
   plugins: [],
 }
